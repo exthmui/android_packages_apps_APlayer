8ca5ad0cf4fcfca3fac10fc78a3c1729107e63fb
cjybyjk <cjybyjk@gmail.com>
2020-07-04 09:49:24 +0800

    Android.mk: use testkey to sign apk

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := APlayer
 LOCAL_OVERRIDES_PACKAGES := Music
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := testkey
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
